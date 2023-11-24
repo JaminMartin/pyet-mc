c82a0323fbf6a9e40dde6466cfe349362f910d89
JaminMartin <jamin.martin1@gmail.com>
2023-11-24 23:57:23 +1300

    started adding plotly functionality

--- src/PYET/helper_funcs.py
+++ src/PYET/helper_funcs.py
@@ -3,10 +3,20 @@ import sys
 import json
 import numpy as np
 import glob
-cache_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'cache'))
+import plotly.graph_objs as go
+import plotly.offline
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+import plotly.io as pio
+pio.templates.default = "none"
 
+cache_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'cache'))
+temp_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'temp'))
 if not os.path.exists(cache_dir):
     os.mkdir(cache_dir)
+if not os.path.exists(temp_dir):
+    os.mkdir(temp_dir)
 
 def cache_writer(r, **params):
 
@@ -99,5 +109,92 @@ if __name__ == "__main__":
     cache_list()
     #cache_clear()
 
-                     
+
+#TODO make this a standalone package
+class Plot:
+    def __init__(self):
+        self.fig = go.Figure()
+        #these are not currently initialised as they need some refining
+        self.default_layout = {
+            'title_text': "",
+            'margin': dict(l=35, r=35, t=35, b=35),
+            'paper_bgcolor': "white",
+            'showlegend': False,
+            'title': '',
+            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
+            'xaxis': dict(
+                title='',
+                exponentformat='none',
+                showgrid=False,
+                showline=True,
+                tickmode='linear',
+                ticks='outside',
+                dtick=50,
+                showticklabels=False,
+                linewidth=4, linecolor='black',
+                ticklen=7.5,
+                tickwidth=4,
+                tickcolor='black',
+                tickfont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black'),
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black',
+                )
+            ),
+            'yaxis': dict(
+                title='Intensity (arb. units.)',
+                exponentformat='none',
+                showgrid=False,
+                zeroline=False,
+                showline=True,
+                ticks='',
+                tickmode='linear',
+                dtick=10,
+                ticklen=15,
+                tickwidth=4,
+                showticklabels=False,
+                linewidth=4, linecolor='black',
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black')
+            )
+        }
+    def scatter_xy(self, x, y, *args, **kwargs):
+        # Set default scatter trace options
+        default_trace_options = {
+            'mode': 'lines',
+            }
+
+        
+        trace_options = {**default_trace_options, **kwargs}
+        trace = go.Scatter(x=x, y=y, *args, **trace_options)
+        self.fig.add_trace(trace)
+
+
+    def show(self, **layout_kwargs):   
+        # Set default layout options
+        final_layout = {**layout_kwargs}
+        self.fig.update_layout(**final_layout)
+
+        plotly.offline.plot(self.fig, filename=f'{cache_dir}/temp_plot.html', auto_open=False)
+
+        app = QApplication(sys.argv)
+
+        web = QWebEngineView()
+
+        file_path = f'{cache_dir}/temp_plot.html'
+
+        web.load(QUrl.fromLocalFile(file_path))
+
+        web.show()
+
+        sys.exit(app.exec())
+
+
+
     
