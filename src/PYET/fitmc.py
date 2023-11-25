8bf1d413ab616163b06afd5bcde915e9b349a5b9
JaminMartin <jamin.martin1@gmail.com>
2023-11-25 22:24:22 +1300

    completed basic documentation

--- src/PYET/fitmc.py
+++ src/PYET/fitmc.py
@@ -8,14 +8,41 @@ import json
 from timeit import default_timer as timer
 import scipy.optimize
 import os
+from typing import Union, Optional, List, Dict, Callable
+
 
 # Model functions for testing and general use
-def test_double_exp(time,dictionary):
- 
+def test_double_exp(time: np.ndarray ,dictionary: Dict) -> np.ndarray:
+    """
+    This function calculates and returns the result of a double exponential function.
+
+    Parameters:
+    time (np.ndarray): The time value used in the exponential calculation.
+    dictionary (dict): A dictionary containing the coefficients used in the calculation. The dictionary should have at least three values.
+    The dictionary contains the parameters that define the exponential A*(e^-p*x - e^-q*x)
+    Amplitude: A
+    First decay rate: p
+    Second decay rate: q
+    Returns:
+    np.ndarray: The result of the calculation as a numpy array.
+    """
     return  list(dictionary.values())[0]*(np.exp(-list(dictionary.values())[1]*time)- np.exp(-list(dictionary.values())[2]*x))
 
-
-def general_energy_transfer(time, radial_data, dictionary):
+# default energy transfer function
+def general_energy_transfer(time: np.ndarray, radial_data:  np.ndarray, dictionary: Dict) -> np.ndarray:
+    """
+    This function calculates and returns the result of a double exponential function.
+    Parameters:
+    time (np.ndarray): The time value used in the exponential calculation.
+    dictionary (dict): A dictionary containing the coefficients used in the calculation. The dictionary should have at least four values.
+    The dictionary contains the parameters that define the exponential A*(e^-Cr*r_i*t - e^-Rad*t)+c
+    Amplitude: A
+    Cross relaxation rate: Cr
+    Radiative relaxation rate: Rad
+    Offset: c
+    Returns:
+    np.ndarray: The result of the calculation as a numpy array.
+    """
     n = len(radial_data)
     exponentials = np.exp(-1 * time[:, np.newaxis] * (list(dictionary.values())[1] * radial_data + list(dictionary.values())[2]))
     result = list(dictionary.values())[0] / n * np.sum(exponentials, axis=1) + list(dictionary.values())[3]
@@ -29,7 +56,26 @@ Actual code
 '''
 #data structure for handling experimental data & radial info
 class Trace:
-    def __init__(self, ydata, xdata, fname, radial_data, parser = False):
+    """
+    The Trace class represents a trace of experimental data points, it allows provides some basic functionalaity to aid in plotting (through naming) and data parsing for manipulating in various ways prior to fitting.
+
+    Attributes:
+    trace (np.ndarray): The y-coordinates of the data points.
+    name (str): The name of the trace.
+    time (np.ndarray): The x-coordinates (time points) of the data points.
+    radial_data (np.ndarray): The radial data associated with the trace, this would be pre-calculated based on the concentration of the sample.
+    """
+    def __init__(self, ydata: np.ndarray, xdata: np.ndarray, fname: str, radial_data: np.ndarray, parser = False):
+        """
+        The constructor for the Trace class.
+
+        Parameters:
+        ydata (np.ndarray): The y-coordinates of the data points.
+        xdata (np.ndarray): The x-coordinates (time points) of the data points.
+        fname (str): The name of the trace.
+        radial_data (np.ndarray): The radial data associated with the trace, this would be pre-calculated based on the concentration of the sample.
+        parser (bool, optional): A flag indicating whether to parse the trace data. Defaults to False.
+        """
         self.trace = ydata
         self.name = fname
         self.time = xdata
@@ -42,9 +88,16 @@ class Trace:
     
 #class for handling the fitting, plotting & logging results 
 class Optimiser:
-     
-    def __init__(self, Traces, variables, model = 'default'):
-      self.Traces = Traces #list of numpy array containing experimental data
+    """
+    The Optimiser class handles the fitting, plotting, and logging of results.
+
+    Attributes:
+    Traces (list): A list of Trace objects containing experimental data.
+    variables (list): A list of variables for each trace.
+    model (function): The model function used to describe the energy transfer process. Defaults to 'general_energy_transfer'.
+    """ 
+    def __init__(self, traces: List[Trace], variables: List[str], model: Union[str, Callable[...,np.ndarray]] = 'default'):
+      self.traces = traces #list of numpy array containing experimental data
       self.variables = variables #list of variables for each trace
       if model == 'default':
           self.model = general_energy_transfer
@@ -53,10 +106,22 @@ class Optimiser:
 
 
 
-    def fit(self, guess,  *args, **kwargs):
+    def fit(self, guess: Dict,  *args, **kwargs) -> Dict:
+        """
+        The fit method performs the fitting process using the provided initial guess and optional arguments.
+        Note this is a decoupled/decoupled fit if multiple traces are provided. It will aim to fit all traces parameters (that are the same) simultanously
+        Parameters:
+        guess (dict): A dictionary containing the initial guess for the parameters.
+        *args: Optional positional arguments passed to the scipy.optimize.minimize function.
+        **kwargs: Optional keyword arguments passed to the scipy.optimize.minimize function.
+
+        Returns:
+        result (OptimizeResult): The result of the optimization process.
+        """
         keys = list(guess.keys())
         print(keys)
         print(f'Guess with initial params:{guess}')
+        print('Started fitting...')
         fn = self.chi
         self.result = scipy.optimize.minimize(
             
@@ -76,9 +141,17 @@ class Optimiser:
 
             
     def chi(self,dictionary):
-        
+        """
+        The chi method calculates the chi-squared value for the current set of parameters.
 
-        total_traces = len(self.Traces)
+        Parameters:
+        dictionary (dict): A dictionary containing the current set of parameters.
+
+        Returns:
+        ch (float): The calculated chi-squared value.
+        """
+
+        total_traces = len(self.traces)
 
         ch = 0
         
@@ -91,7 +164,7 @@ class Optimiser:
             #print(temp_dict)
 
             #temp_dict2 ={k:v for k,v in zip(keys, result.x)}
-            ch += np.sum(((self.model(self.Traces[j].time,self.Traces[j].radial_data, temp_dict) - self.Traces[j].trace)**2))
+            ch += np.sum(((self.model(self.traces[j].time, self.traces[j].radial_data, temp_dict) - self.traces[j].trace)**2))
             
         #print(ch)
         return ch
