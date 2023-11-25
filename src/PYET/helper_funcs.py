a8ae2dfa05f3abde0bc896e24fb47c66f4818c09
JaminMartin <jamin.martin1@gmail.com>
2023-11-25 23:55:19 +1300

    moded multiprocessing for plotting

--- src/PYET/helper_funcs.py
+++ src/PYET/helper_funcs.py
@@ -118,59 +118,62 @@ def run_app(file_path):
 
 #TODO make this a standalone package
 class Plot:
-    def __init__(self):
+    def __init__(self, **layout_kwargs):
         self.fig = go.Figure()
         #these are not currently initialised as they need some refining
         # TODO find some good defaults & window size for plots that couple well to plotting & saving for standard journal formats. 
-        self.default_layout = {
-            'title_text': "",
-            'margin': dict(l=35, r=35, t=35, b=35),
-            'paper_bgcolor': "white",
-            'showlegend': False,
-            'title': '',
-            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
-            'xaxis': dict(
-                title='',
-                exponentformat='none',
-                showgrid=False,
-                showline=True,
-                tickmode='linear',
-                ticks='outside',
-                dtick=50,
-                showticklabels=False,
-                linewidth=4, linecolor='black',
-                ticklen=7.5,
-                tickwidth=4,
-                tickcolor='black',
-                tickfont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black'),
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black',
-                )
-            ),
-            'yaxis': dict(
-                title='Intensity (arb. units.)',
-                exponentformat='none',
-                showgrid=False,
-                zeroline=False,
-                showline=True,
-                ticks='',
-                tickmode='linear',
-                dtick=10,
-                ticklen=15,
-                tickwidth=4,
-                showticklabels=False,
-                linewidth=4, linecolor='black',
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black')
-            )
-        }
+        # self.default_layout = {
+        #     'title_text': "",
+        #     'margin': dict(l=35, r=35, t=35, b=35),
+        #     'paper_bgcolor': "white",
+        #     'showlegend': False,
+            # 'title': '',
+            # 'font': dict(family='Times New Roman, monospace', size=20, color='black'),
+            # 'xaxis': dict(
+            #     title='',
+            #     exponentformat='none',
+            #     showgrid=False,
+            #     showline=True,
+            #     tickmode='linear',
+            #     ticks='outside',
+            #     dtick=50,
+            #     showticklabels=False,
+            #     linewidth=4, linecolor='black',
+            #     ticklen=7.5,
+            #     tickwidth=4,
+            #     tickcolor='black',
+            #     tickfont=dict(
+            #         family='Times new roman, monospace',
+            #         size=20,
+            #         color='black'),
+            #     titlefont=dict(
+            #         family='Times new roman, monospace',
+            #         size=20,
+            #         color='black',
+            #     )
+            # ),
+        #     'yaxis': dict(
+        #         title='Intensity (arb. units.)',
+        #         exponentformat='none',
+        #         showgrid=False,
+        #         zeroline=False,
+        #         showline=True,
+        #         ticks='',
+        #         tickmode='linear',
+        #         dtick=10,
+        #         ticklen=15,
+        #         tickwidth=4,
+        #         showticklabels=False,
+        #         linewidth=4, linecolor='black',
+        #         titlefont=dict(
+        #             family='Times new roman, monospace',
+        #             size=20,
+        #             color='black')
+        #     )
+        # }
+        self.default_layout = {} 
+        self.default_layout.update(layout_kwargs)
+
     def scatter_xy(self, x, y, *args, **kwargs):
         # Set default scatter trace options
         default_trace_options = {
@@ -183,10 +186,9 @@ class Plot:
         self.fig.add_trace(trace)
 
 
-    def show(self, **layout_kwargs):
+    def show(self):
         # Set default layout options
-        final_layout = {**layout_kwargs}
-        self.fig.update_layout(**final_layout)
+        self.fig.update_layout(**self.default_layout)
 
         with tempfile.NamedTemporaryFile(suffix=".html", dir=temp_dir, delete=False) as temp:
             plotly.offline.plot(self.fig, filename=temp.name, auto_open=False)
@@ -198,6 +200,11 @@ class Plot:
         if hasattr(self, 'temp_file_path'):
             os.remove(self.temp_file_path)
 
+    
+     
+    #TODO add figure saving 
+
+
 
 if __name__ == "__main__":
     #data = cache_reader(process = 'singlecross', radius = 10 , concentration = 2.5 , iterations = 50000 , interaction_type = 'QQ')
@@ -205,6 +212,16 @@ if __name__ == "__main__":
     #cache_clear()    
     x = [0,2,3,4,6,7,8,9,10]
     y = [11,12,13,14,15,16,17,18,19 ]
+    x2 = [5,6,74,8,99,83,91,100]
+    y2 = [11,12,13,14,15,16,17,18,19 ]
     figure = Plot()
     figure.scatter_xy(x,y)
+    figure.scatter_xy(x2,y2)
     figure.show()
+
+
+
+
+    figure2 = Plot()
+    figure2.scatter_xy(x2,y2)
+    figure2.show()
