de554741fffb5eff0dd63dc476cc65f664c1eda9
JaminMartin <jamin.martin1@gmail.com>
2023-11-26 19:00:50 +1300

    added transient method to plotting WIP

--- src/PYET/helper_funcs.py
+++ src/PYET/helper_funcs.py
@@ -11,7 +11,6 @@ from PyQt5.QtWidgets import QApplication
 import plotly.io as pio
 from multiprocessing import Process
 import tempfile
-
 pio.templates.default = "none"
 
 cache_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'cache'))
@@ -20,6 +19,17 @@ if not os.path.exists(cache_dir):
     os.mkdir(cache_dir)
 if not os.path.exists(temp_dir):
     os.mkdir(temp_dir)
+
+def cleanup_temp_folder(temp_folder_path):
+    for filename in os.listdir(temp_folder_path):
+        if filename.endswith(".html"):
+            file_path = os.path.join(temp_folder_path, filename)
+            try:
+                os.remove(file_path)
+            except Exception as e:
+                print(f"Error while removing {file_path}: {e}")
+
+
 
 def cache_writer(r, **params):
 
@@ -115,64 +125,141 @@ def run_app(file_path):
     web.show()
     app.exec_()
 
+#data structure for handling experimental data & radial info
+class Trace:
+    """
+    The Trace class represents a trace of experimental data points, it allows provides some basic functionalaity to aid in plotting (through naming) and data parsing for manipulating in various ways prior to fitting.
+
+    Attributes:
+    trace (np.ndarray): The y-coordinates of the data points.
+    name (str): The name of the trace.
+    time (np.ndarray): The x-coordinates (time points) of the data points.
+    radial_data (np.ndarray): The radial data associated with the trace, this would be pre-calculated based on the concentration of the sample.
+    """
+    def __init__(self, ydata: np.ndarray, xdata: np.ndarray, fname: str, radial_data: np.ndarray, parser = False):
+        """
+        The constructor for the Trace class.
+
+        Parameters:
+        ydata (np.ndarray): The y-coordinates of the data points.
+        xdata (np.ndarray): The x-coordinates (time points) of the data points.
+        fname (str): The name of the trace.
+        radial_data (np.ndarray): The radial data associated with the trace, this would be pre-calculated based on the concentration of the sample.
+        parser (bool, optional): A flag indicating whether to parse the trace data. Defaults to False.
+        """
+        self.trace = ydata
+        self.name = fname
+        self.time = xdata
+        self.radial_data = radial_data
+        if parser == True:
+            self.trace = self.parse(self.trace)
+
+        def parse(self, trace):
+            print('not yet implemented')  
+
 
 #TODO make this a standalone package
 class Plot:
     def __init__(self, **layout_kwargs):
         self.fig = go.Figure()
+        self.plot_type = 'default'
         #these are not currently initialised as they need some refining
         # TODO find some good defaults & window size for plots that couple well to plotting & saving for standard journal formats. 
-        # self.default_layout = {
-        #     'title_text': "",
-        #     'margin': dict(l=35, r=35, t=35, b=35),
-        #     'paper_bgcolor': "white",
-        #     'showlegend': False,
-            # 'title': '',
-            # 'font': dict(family='Times New Roman, monospace', size=20, color='black'),
-            # 'xaxis': dict(
-            #     title='',
-            #     exponentformat='none',
-            #     showgrid=False,
-            #     showline=True,
-            #     tickmode='linear',
-            #     ticks='outside',
-            #     dtick=50,
-            #     showticklabels=False,
-            #     linewidth=4, linecolor='black',
-            #     ticklen=7.5,
-            #     tickwidth=4,
-            #     tickcolor='black',
-            #     tickfont=dict(
-            #         family='Times new roman, monospace',
-            #         size=20,
-            #         color='black'),
-            #     titlefont=dict(
-            #         family='Times new roman, monospace',
-            #         size=20,
-            #         color='black',
-            #     )
-            # ),
-        #     'yaxis': dict(
-        #         title='Intensity (arb. units.)',
-        #         exponentformat='none',
-        #         showgrid=False,
-        #         zeroline=False,
-        #         showline=True,
-        #         ticks='',
-        #         tickmode='linear',
-        #         dtick=10,
-        #         ticklen=15,
-        #         tickwidth=4,
-        #         showticklabels=False,
-        #         linewidth=4, linecolor='black',
-        #         titlefont=dict(
-        #             family='Times new roman, monospace',
-        #             size=20,
-        #             color='black')
-        #     )
-        # }
-        self.default_layout = {} 
+        self.default_layout = {
+            'title_text': "",
+            'margin': dict(l=35, r=35, t=35, b=35),
+            'paper_bgcolor': "white",
+            'showlegend': True,
+            'title': '',
+            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
+            'xaxis': dict(
+                title='',
+                exponentformat='none',
+                showgrid=False,
+                showline=True,
+                tickmode='linear',
+                ticks='outside',
+                dtick=50,
+                showticklabels=False,
+                linewidth=4, linecolor='black',
+                ticklen=7.5,
+                tickwidth=4,
+                tickcolor='black',
+                tickfont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black'),
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black',
+                )
+            ),
+            'yaxis': dict(
+                title='Intensity (arb. units.)',
+                exponentformat='none',
+                showgrid=False,
+                zeroline=False,
+                showline=True,
+                ticks='',
+                tickmode='linear',
+                dtick=10,
+                ticklen=15,
+                tickwidth=4,
+                showticklabels=False,
+                linewidth=4, linecolor='black',
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black')
+            )
+        } 
         self.default_layout.update(layout_kwargs)
+        self.default_transient_layout = {
+            'title_text': "",
+            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
+            'xaxis': dict(
+                title='Time (ms)',
+                exponentformat='none',
+                showgrid=False,
+                showline=True,
+                zeroline=False,
+                range=[0,0.01],
+                tickmode='linear',
+                ticks='outside',
+                tick0=0,
+                dtick=0.01,
+                linewidth=4, linecolor='black',
+                ticklen=15,
+                tickwidth=4,
+                tickcolor='black',
+                tickfont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black'),
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black')
+            ),
+            'yaxis': dict(
+                title='Intensity (arb. units.)',
+                exponentformat='none',
+                showgrid=False,
+                zeroline=False,
+                showline=True,
+                ticks='',
+                #range=[np.log(0.015), np.log(1)],
+                showticklabels=False,
+                linewidth=4, linecolor='black',
+                titlefont=dict(
+                    family='Times new roman, monospace',
+                    size=20,
+                    color='black')
+            )
+        }
+        self.default_layout.update(layout_kwargs)
+
 
     def scatter_xy(self, x, y, *args, **kwargs):
         # Set default scatter trace options
@@ -186,9 +273,37 @@ class Plot:
         self.fig.add_trace(trace)
 
 
+    def transient(self, x, y=None, fit=False, *args, **kwargs):
+        self.plot_type = 'transient'
+        if fit:
+            default_trace_options = {'mode': 'lines'}
+        else:
+            default_trace_options = {'mode': 'markers'}
+
+        trace_options = {**default_trace_options, **kwargs}
+
+        # Check if x is a Trace instance
+        if isinstance(x, Trace):
+            trace = go.Scatter(x=x.time, y=x.trace, name=x.name, *args, **trace_options)
+        else:
+            trace = go.Scatter(x=x, y=y, *args, **trace_options)
+
+        self.fig.add_trace(trace)
+
+    def cleanup_temp_file(self):
+        if hasattr(self, 'temp_file_path'):
+            os.remove(self.temp_file_path)
+
     def show(self):
-        # Set default layout options
-        self.fig.update_layout(**self.default_layout)
+        if self.plot_type == 'default':
+            # Set default layout options
+            self.fig.update_layout(**self.default_layout)
+        elif self.plot_type == 'transient':
+            self.fig.update_layout(**self.default_transient_layout) 
+            self.fig.update_yaxes(type="log")   
+        else:
+            print('invalid plot type, defaulting to default')  
+            self.fig.update_layout(**self.default_layout)  
 
         with tempfile.NamedTemporaryFile(suffix=".html", dir=temp_dir, delete=False) as temp:
             plotly.offline.plot(self.fig, filename=temp.name, auto_open=False)
@@ -197,8 +312,7 @@ class Plot:
             self.process.start()
 
     def __del__(self):
-        if hasattr(self, 'temp_file_path'):
-            os.remove(self.temp_file_path)
+        self.cleanup_temp_file()
 
     
      
@@ -215,13 +329,13 @@ if __name__ == "__main__":
     x2 = [5,6,74,8,99,83,91,100]
     y2 = [11,12,13,14,15,16,17,18,19 ]
     figure = Plot()
-    figure.scatter_xy(x,y)
-    figure.scatter_xy(x2,y2)
+    figure.transient(x,y)
+    figure.transient(x2,y2)
     figure.show()
 
 
 
 
     figure2 = Plot()
-    figure2.scatter_xy(x2,y2)
+    figure2.transient(x2,y2, fit = True)
     figure2.show()
