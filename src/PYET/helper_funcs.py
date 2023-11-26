481d74eddc6c5dac15acc99c3084cc3f3c54c6a4
JaminMartin <jamin.martin1@gmail.com>
2023-11-27 10:00:08 +1300

    now using sytem temp files and config.toml

--- src/PYET/helper_funcs.py
+++ src/PYET/helper_funcs.py
@@ -12,23 +12,16 @@ import plotly.io as pio
 from multiprocessing import Process
 import tempfile
 pio.templates.default = "none"
+import toml
 
 cache_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'cache'))
-temp_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'temp'))
+config_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'config'))
+
 if not os.path.exists(cache_dir):
     os.mkdir(cache_dir)
-if not os.path.exists(temp_dir):
-    os.mkdir(temp_dir)
 
-def cleanup_temp_folder(temp_folder_path):
-    for filename in os.listdir(temp_folder_path):
-        if filename.endswith(".html"):
-            file_path = os.path.join(temp_folder_path, filename)
-            try:
-                os.remove(file_path)
-            except Exception as e:
-                print(f"Error while removing {file_path}: {e}")
-
+with open(f"{config_dir}/plotting_config.toml", "r") as f:
+        config = toml.load(f)
 
 
 def cache_writer(r, **params):
@@ -165,99 +158,9 @@ class Plot:
         self.plot_type = 'default'
         #these are not currently initialised as they need some refining
         # TODO find some good defaults & window size for plots that couple well to plotting & saving for standard journal formats. 
-        self.default_layout = {
-            'title_text': "",
-            'margin': dict(l=35, r=35, t=35, b=35),
-            'paper_bgcolor': "white",
-            'showlegend': True,
-            'title': '',
-            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
-            'xaxis': dict(
-                title='',
-                exponentformat='none',
-                showgrid=False,
-                showline=True,
-                tickmode='linear',
-                ticks='outside',
-                dtick=50,
-                showticklabels=False,
-                linewidth=4, linecolor='black',
-                ticklen=7.5,
-                tickwidth=4,
-                tickcolor='black',
-                tickfont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black'),
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black',
-                )
-            ),
-            'yaxis': dict(
-                title='Intensity (arb. units.)',
-                exponentformat='none',
-                showgrid=False,
-                zeroline=False,
-                showline=True,
-                ticks='',
-                tickmode='linear',
-                dtick=10,
-                ticklen=15,
-                tickwidth=4,
-                showticklabels=False,
-                linewidth=4, linecolor='black',
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black')
-            )
-        } 
+        self.default_layout = config.get("default_layout", {})
         self.default_layout.update(layout_kwargs)
-        self.default_transient_layout = {
-            'title_text': "",
-            'font': dict(family='Times New Roman, monospace', size=20, color='black'),
-            'xaxis': dict(
-                title='Time (ms)',
-                exponentformat='none',
-                showgrid=False,
-                showline=True,
-                zeroline=False,
-                range=[0,0.01],
-                tickmode='linear',
-                ticks='outside',
-                tick0=0,
-                dtick=0.01,
-                linewidth=4, linecolor='black',
-                ticklen=15,
-                tickwidth=4,
-                tickcolor='black',
-                tickfont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black'),
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black')
-            ),
-            'yaxis': dict(
-                title='Intensity (arb. units.)',
-                exponentformat='none',
-                showgrid=False,
-                zeroline=False,
-                showline=True,
-                ticks='',
-                #range=[np.log(0.015), np.log(1)],
-                showticklabels=False,
-                linewidth=4, linecolor='black',
-                titlefont=dict(
-                    family='Times new roman, monospace',
-                    size=20,
-                    color='black')
-            )
-        }
+        self.default_transient_layout = config.get("transient_layout", {})
         self.default_layout.update(layout_kwargs)
 
 
@@ -266,8 +169,6 @@ class Plot:
         default_trace_options = {
             'mode': 'lines',
             }
-
-        
         trace_options = {**default_trace_options, **kwargs}
         trace = go.Scatter(x=x, y=y, *args, **trace_options)
         self.fig.add_trace(trace)
@@ -290,9 +191,6 @@ class Plot:
 
         self.fig.add_trace(trace)
 
-    def cleanup_temp_file(self):
-        if hasattr(self, 'temp_file_path'):
-            os.remove(self.temp_file_path)
 
     def show(self):
         if self.plot_type == 'default':
@@ -303,39 +201,50 @@ class Plot:
             self.fig.update_yaxes(type="log")   
         else:
             print('invalid plot type, defaulting to default')  
-            self.fig.update_layout(**self.default_layout)  
+            self.fig.update_layout(**self.default_layout) 
 
+    
+        # Use the system's temporary directory
+        temp_dir = tempfile.gettempdir()
+        print(f"Temporary directory for debugging: {temp_dir}")
+
+        # Use NamedTemporaryFile to create a temporary file in the system's temp directory
         with tempfile.NamedTemporaryFile(suffix=".html", dir=temp_dir, delete=False) as temp:
-            plotly.offline.plot(self.fig, filename=temp.name, auto_open=False)
             self.temp_file_path = os.path.abspath(temp.name)
+            plotly.offline.plot(self.fig, filename=self.temp_file_path, auto_open=False)
+
+            # Start the process using self.temp_file_path
             self.process = Process(target=run_app, args=(self.temp_file_path,))
             self.process.start()
 
+    #First layer temp file clean up (as these can be quite large) system level clean up handles any python process / garbage collection failure at reboot (rarely needed)
     def __del__(self):
         self.cleanup_temp_file()
 
-    
-     
+    def cleanup_temp_file(self):
+        if hasattr(self, 'temp_file_path') and os.path.exists(self.temp_file_path):
+            os.remove(self.temp_file_path)
+
     #TODO add figure saving 
 
 
 
 if __name__ == "__main__":
-    #data = cache_reader(process = 'singlecross', radius = 10 , concentration = 2.5 , iterations = 50000 , interaction_type = 'QQ')
+    
     cache_list()
-    #cache_clear()    
+     
     x = [0,2,3,4,6,7,8,9,10]
     y = [11,12,13,14,15,16,17,18,19 ]
     x2 = [5,6,74,8,99,83,91,100]
     y2 = [11,12,13,14,15,16,17,18,19 ]
     figure = Plot()
-    figure.transient(x,y)
-    figure.transient(x2,y2)
+    figure.scatter_xy(x,y)
+    figure.scatter_xy(x2,y2)
     figure.show()
 
 
 
 
     figure2 = Plot()
-    figure2.transient(x2,y2, fit = True)
+    figure2.scatter_xy(x2,y2)
     figure2.show()
