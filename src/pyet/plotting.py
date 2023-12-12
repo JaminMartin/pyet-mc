b4b1ecfd4e1d64ecbc1eb685751757748e828ee4
JaminMartin <jamin.martin1@gmail.com>
2023-12-12 16:28:50 +1300

    plotting not supports a local config file

--- src/pyet/plotting.py
+++ src/pyet/plotting.py
@@ -28,18 +28,30 @@ def run_app(file_path):
     web.show()
     app.exec_()
 
+def load_local_config(local_config_path):
+    """
+    Load a local configuration file and overwrite the default configuration.
 
+    Parameters:
+    local_config_path (str): The path to the local configuration file.
+    """
+
+    # Load the local configuration file
+    local_config = toml.load(local_config_path)
+
+    # Update the default configuration with the local configuration
+    config.update(local_config)
 
 
 class Plot:
-    def __init__(self, path = None, **layout_kwargs):
+    def __init__(self, **layout_kwargs):
         self.fig = go.Figure()
         self.plot_type = 'default'
         self.default_layout = copy.deepcopy(config.get("default_layout", {}))
         self.default_transient_layout = copy.deepcopy(config.get("transient_layout", {}))
         self.default_scatter3d_layout = copy.deepcopy(config.get("structure_3d_layout", {}))
 
-        # Rest of your initialization code...
+        self.layout = layout_kwargs
 
 
         # Update any specified keys in the default layout with the provided values
@@ -116,7 +128,7 @@ class Plot:
 
     def calculate_nice_round_number(self, value):
         # Find a nice round number or factor of value
-        nice_round_numbers = [0.01, 0.02, 0.05, 0.1, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]  # You can extend this list with more nice round numbers
+        nice_round_numbers = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]  # You can extend this list with more nice round numbers
         for nice_number in nice_round_numbers:
             if value / nice_number <= 5:  # Adjust the threshold as needed
                 return nice_number
@@ -125,38 +137,39 @@ class Plot:
         return 1
     
     def update_layout_bounds(self):
-        x_max, y_max = float('-inf'), float('-inf')
+        x_min, x_max = float('inf'), float('-inf')
+        y_min, y_max = float('inf'), float('-inf')
 
         for trace in self.fig.data:
             # Check if x is not empty or None
             if trace.x is not None and len(trace.x) > 0:
                 # Update x-axis bounds
+                x_min = min(x_min, min(trace.x))
                 x_max = max(x_max, max(trace.x))
 
             # Check if y is not empty or None
             if trace.y is not None and len(trace.y) > 0:
                 # Update y-axis bounds
+                y_min = min(y_min, min(trace.y))
                 y_max = max(y_max, max(trace.y))
-
-    
-        t_max = x_max
-        t_min = 0
-        y_max = 1
-        y_min = 0.1 * y_max
-                
-        self.default_transient_layout['yaxis']['range'] = [np.log(y_min), np.log(y_max)]
-        self.default_transient_layout['xaxis']['range'] = [t_min, t_max]
-        
-        t_range = t_max - t_min
-        dtick = self.calculate_nice_round_number(t_range)
-        self.default_transient_layout['xaxis']['dtick'] = dtick
-        
-
+            
+                # Only update the layout bounds if they are not set in layout_kwargs
+            if 'xaxis' not in self.layout or 'range' not in self.layout['xaxis']:
+                self.default_transient_layout['xaxis']['range'] = [0, x_max]
+                self.default_layout['xaxis']['range'] = [x_min, x_max]
+            if 'yaxis' not in self.layout or 'range' not in self.layout['yaxis']:
+                self.default_transient_layout['yaxis']['range'] = [np.log(0.1), np.log(1)]
+            if 'xaxis' not in self.layout or 'dtick' not in self.layout['xaxis']:
+                x_range = x_max - x_min
+                dtick = self.calculate_nice_round_number(x_range)
+                self.default_transient_layout['xaxis']['dtick'] = dtick
+                self.default_layout['xaxis']['dtick'] = dtick    
 
 
     def show(self):
         if self.plot_type == 'default':
             # Set default layout options
+            self.update_layout_bounds()
             self.fig.update_layout(**self.default_layout)
 
         elif self.plot_type == 'transient':
@@ -198,13 +211,13 @@ class Plot:
         
 
 if __name__ == "__main__":
-    margins = {'l': 30, 'r': 0, 't': 30, 'b': 30}
-    figure = Plot(margin = margins)
+    # margins = {'l': 30, 'r': 0, 't': 30, 'b': 30}
+    # figure = Plot(margin = margins)
     
-    figure.structure_3d([0,1,2],[0,5,2],[0,1,5])
-    figure.structure_3d([5,1,1],[2,0,1],[2,2,1], name='yttrium', marker={'size': 10})
-    figure.show()
-     
+    # figure.structure_3d([0,1,2],[0,5,2],[0,1,5])
+    # figure.structure_3d([5,1,1],[2,0,1],[2,2,1], name='yttrium', marker={'size': 10})
+    # figure.show()
+    margins = {'l': 30, 'r': 0, 't': 30, 'b': 30} 
     x = [0,2,3,4,6,7,8,9,10]
     y = [11,12,13,14,15,16,17,18,19 ]
     x2 = [5,6,74,8,99,83,91,100]
@@ -217,7 +230,7 @@ if __name__ == "__main__":
     figure2.scatter_xy(x, y)
     figure2.show()
 
-
+    #load_local_config('/Users/jamin/Documents/PYET/pyet-mc/local_plotting_config.toml')
     figure1 = Plot()
     figure1.scatter_xy(x, y)
     figure1.show()
